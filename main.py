3f1bd1a54c87465d181954b2ef5b3e2aae21d024
yudha.alfian <yudhanurulalfian@gmail.com>
2022-11-23 07:16:48 +0700

    dimension_tolerance

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-# macro to create tolerance dimension for powershape
+"""
+pshape = GetActiveObject("PowerShape.Application")
+pshape.Visible = True
+# Connect to PowerShape
+# pshape = window.external;
+pshape.Exec('LEVEL FILTER USED_OFF')
+LEVEL_ON = pshape.Evaluate('level.filtered.number')
+pymsgbox.alert(LEVEL_ON)
+"""
+from comtypes.client import GetActiveObject
+import pymsgbox
+
+pshape = GetActiveObject("PowerShape.Application")
